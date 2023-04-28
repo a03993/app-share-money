8eac92d133d9a55a3b48fdd77872317008e12e82
a03993 <a03993@yahoo.com.tw>
2023-04-28 14:48:23 +0800

    page Result update

--- src/components/Main/Result/index.jsx
+++ src/components/Main/Result/index.jsx
@@ -1,30 +1,126 @@
 import React, { useState } from 'react';
+import { paymentDataTest, paymentResultDataTest, totalPriceTest, paymentAverageTest } from '../data'
 import { numberWithCommas } from '../function'
 
 import Style from "./Result.module.css"
 
 function Result({ paymentData, totalPrice }) {
 
-    const [checked, setChecked] = useState(false);
-   
-    const paymentResultData = [
-        {},
-        {}
-    ]
+    const paymentResult = []
+    const creditor = []
+    const debtor = []
 
-    // person avatar list rendering
-    const ShowPersonAvatar = paymentData.map (person => 
+    // 從 paymentData(Test) render 使用者的 avatar 
+    const ShowPersonAvatar = paymentDataTest.map (person => 
     <li key={person.payment.id} style={{background: person.color}}>{person.name[0]}</li>)
 
-    // 平均一人應付價錢
-    const SumAverage = numberWithCommas(Math.round(totalPrice/paymentData.length))
+    function Classification() {
+        for(let i = 0; i < paymentResultDataTest.length; i++){
+            if(paymentAverageTest - paymentResultDataTest[i].payment < 0){
+                // console.log(paymentResultDataTest[i].name + '不用付錢，需要拿回$' + Math.abs(paymentAverageTest - paymentResultDataTest[i].payment))
+                creditor.push({
+                    name: paymentResultDataTest[i].name,
+                    color: paymentResultDataTest[i].color,
+                    bePayed: Math.round(Math.abs(paymentAverageTest - paymentResultDataTest[i].payment))
+                })
+            } else {
+                // console.log(paymentResultDataTest[i].name + '需要付出$' + Math.abs(paymentAverageTest - paymentResultDataTest[i].payment))
+                debtor.push({
+                    name: paymentResultDataTest[i].name,
+                    color: paymentResultDataTest[i].color,
+                    pay: Math.round(Math.abs(paymentAverageTest - paymentResultDataTest[i].payment))
+                })
+            }
+        }
+    }
 
+    Classification()
+
+    function toPay(){
+        for(let i = 0; i < debtor.length; i++){
+            paymentResult.push({
+                debtor:debtor[i].name,
+                color:debtor[i].color,
+                payment:[]
+            })
+            for(let j = 0; j < creditor.length; j++){
+                if (debtor[i].pay - creditor[j].bePayed > 0){
+                    console.log(debtor[i].name + ' need to pay ' + creditor[j].name + creditor[j].bePayed + '元')
+                    paymentResult[i].payment.push({
+                        creditor: creditor[j].name,
+                        color: creditor[j].color,
+                        debt: creditor[j].bePayed
+                    })
+                    debtor[i].pay = debtor[i].pay - creditor[j].bePayed
+                    creditor[j].bePayed = 0
+                    console.log(debtor[i].name + ' 還要繼續還下一個人錢')    
+                } else if (debtor[i].pay - creditor[j].bePayed === 0){
+                    console.log(debtor[i].name + ' need to pay ' + creditor[j].name + creditor[j].bePayed + '元')
+                    paymentResult[i].payment.push({
+                        creditor: creditor[j].name,
+                        color: creditor[j].color,
+                        debt: creditor[j].bePayed
+                    })
+                    debtor[i].pay = 0
+                    creditor[j].bePayed = 0
+                    console.log(debtor[i].name + '不用再還錢')
+                    break
+                } else if (debtor[i].pay - creditor[j].bePayed < 0){
+                    console.log(debtor[i].name + ' need to pay ' + creditor[j].name + debtor[i].pay + '元')
+                    paymentResult[i].payment.push({
+                        creditor: creditor[j].name,
+                        color: creditor[j].color,
+                        debt: debtor[i].pay
+                    })
+                    creditor[j].bePayed = creditor[j].bePayed - debtor[i].pay
+                    debtor[i].pay = 0
+                    console.log(creditor[j].name + '還需得到' + creditor[j].bePayed)
+                    console.log(debtor[i].name + '不用再還錢')
+                    break
+                }
+            }
+        }
+    }
+
+    toPay()
+    console.log('paymentResult: ',paymentResult)
+
+    const Payment = paymentResult.map(person =>(
+        <>
+            <div className={Style.payment__wrapper}>
+                <div className={Style.payment__list}>
+                    <div>
+                        <div className={Style.avatar} style={{background: person.color}}>{person.debtor[0]}</div>
+                    </div>
+                    <div className={Style.payment__content}>
+                        <ul >
+                            {person.payment.map(payment =>(
+                                <li className={Style.payment}>
+                                    <div className={Style.pay}>
+                                        <div>pay</div>
+                                        <div className={Style.pay__arrow}></div>
+                                    </div>
+                                    <div className={Style.avatar}  style={{background: payment.color}}>{payment.creditor[0]}</div>
+                                    <div className={Style.price}>${numberWithCommas(payment.debt)}</div>
+                                    <label>
+                                        <input className={Style.checkPayment} type="checkbox" name="check"></input>
+                                        <span></span>
+                                    </label>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>          
+            </div>
+            <div className={Style.line}></div>
+        </>
+    ))
 
     // checkbox
-    const handleChange = (event) => {
-        setChecked(event.target.checked);
-        event.stopPropagation();
-    };
+    // const handleChange = (event) => {
+    //     setChecked(event.target.checked);
+    //     event.stopPropagation();
+    // };
 
     return(
         <div className={Style.container}>
@@ -32,18 +128,18 @@ function Result({ paymentData, totalPrice }) {
             <div className={Style.average__container}>
                 <div className={Style.average__wrapper}>
                     <div className={Style.average}>
-                        <h1>${SumAverage}</h1>
+                        <h1>${numberWithCommas(paymentAverageTest)}</h1>
                         <h3>/人</h3>
                     </div>
                 </div>
                 <div className={Style.total__list__container}>
                     <div className={Style.total__wrapper}>
                         <div className={Style.total}>
-                            <h3>{paymentData.length}</h3>
+                            <h3>{paymentDataTest.length}</h3>
                             <h4>人</h4>
                         </div>
                         <div className={Style.total__spending}>
-                            <h3>${numberWithCommas(totalPrice)}</h3>
+                            <h3>${numberWithCommas(totalPriceTest)}</h3>
                         </div>       
                     </div>
                     <div className={Style.avatar__list__wrapper}>
@@ -59,138 +155,16 @@ function Result({ paymentData, totalPrice }) {
                 <div className={Style.payment__list__wrapper}>
                     <h2>Payment</h2>
                     <div className={Style.line}></div>
-                    
-                    {/* <ul className={Style.payment__list}>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar} style={{background: 'var(--avatar-color-3)'}}>B</div>
-                            <div className={Style.pay}>
-                                <div>pay</div>
-                                <div className={Style.pay__arrow}></div>
-                            </div>
-                            <div className={Style.avatar}  style={{background: 'var(--avatar-color-2)'}}>M</div>
-                            <div className={Style.price} style={{color: checked === true ? 'var(--done-color)' : 'var(--primary-text)'}}>$42</div>
-                            <label>
-                                <input className={Style.checkPayment} type="checkbox" name="check" id={uuid()}></input>
-                                <span></span>
-                            </label>
-                        </li>
-                    </ul> 
-                    <div className={Style.line}></div>
-
-                    <ul className={Style.payment__list}>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-4)'}}>T</div>
-                            <div className={Style.pay}>
-                                <div>pay</div>
-                                <div className={Style.pay__arrow}></div>
-                            </div>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-3)'}}>B</div>
-                            <div className={Style.price}>$79</div>
-                            <label>
-                                <input className={Style.checkPayment} type="checkbox" name="check"></input>
-                                <span></span>
-                            </label>
-                        </li>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar__hidden} id="avatar__pay" style={{background: 'var(--avatar-color-4)'}}>T</div>
-                            <div className={Style.pay}>
-                            <div>pay</div>
-                                <div className={Style.pay__arrow}></div>
-                            </div>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-5)'}}>C</div>
-                            <div className={Style.price}>$50</div>
-                            <label>
-                                <input className={Style.checkPayment} type="checkbox" name="check"></input>
-                                <span></span>
-                            </label>
-                        </li>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar__hidden} id="avatar__pay" style={{background: 'var(--avatar-color-4)'}}>T</div>
-                            <div className={Style.pay}>
-                                <div>pay</div>
-                                <div className={Style.pay__arrow}></div>
-                            </div>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-2)'}}>M</div>
-                            <div className={Style.price}>$121</div>
-                            <label>
-                                <input className={Style.checkPayment} type="checkbox" name="check"></input>
-                                <span></span>
-                            </label>
-                        </li>
-                    </ul>         
-                    <div className={Style.line}></div>
-                    
-                    <ul className={Style.payment__list}>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-5)'}}>C</div>
-                            <div className={Style.pay}>
-                                <div>pay</div>
-                                <div className={Style.pay__arrow}></div>
-                            </div>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-3)'}}>B</div>
-                            <div className={Style.price}>$29</div>
-                            <label>
-                                <input className={Style.checkPayment} type="checkbox" name="check"></input>
-                                <span></span>
-                            </label>
-                        </li>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar__hidden} id="avatar__pay" style={{background: 'var(--avatar-color-5)'}}>C</div>
-                            <div className={Style.pay}>
-                                <div>pay</div>
-                                <div className={Style.pay__arrow}></div>
-                            </div>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-2)'}}>M</div>
-                            <div className={Style.price}>$71</div>
-                            <label>
-                                <input className={Style.checkPayment} type="checkbox" name="check"></input>
-                                <span></span>
-                            </label>
-                        </li>
-                    </ul>
-                    <div className={Style.line}></div>
-                    
-                    <ul className={Style.payment__list}>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-2)'}}>M</div>
-                            <h3>Don't need to pay anyone</h3>
-                        </li>
-                    </ul>
-                    <div className={Style.line}></div>         */}
+                    {Payment}
                 </div>
 
                 {/* done payment */}
                 {/* show on desktop */}
-                {/* <div className={Style.done__payment__list__wrapper}>
+                <div className={Style.done__payment__list__wrapper}>
                     <h2>Done Payment</h2>
                     <div className={Style.line}></div>
-                    
-                    <ul className={Style.payment__list}>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar} style={{background: 'var(--avatar-color-3)'}}>B</div>
-                            <div className={Style.pay}>
-                                <div style={{color:'var(--done-color)'}}>pay</div>
-                                <div className={Style.pay__arrow__done}></div>
-                            </div>
-                            <div className={Style.avatar} style={{background: 'var(--avatar-color-2)'}}>M</div>
-                            <div className={Style.price} style={{color:'var(--done-color)'}}>$42</div>
-                            <label>
-                                <input className={Style.checkPayment} type="checkbox" name="check" defaultChecked></input>
-                                <span></span>
-                            </label>
-                        </li>
-                    </ul> 
-                    <div className={Style.line}></div>
-                    
-                    <ul className={Style.payment__list}>
-                        <li className={Style.payment}>
-                            <div className={Style.avatar} id="avatar__pay" style={{background: 'var(--avatar-color-2)'}}>M</div>
-                            <h3>Don't need to pay anyone</h3>
-                        </li>
-                    </ul>
-                    <div className={Style.line}></div>        
-                </div>      */}
-
+                        
+                    </div>     
             </div>
         </div>
     )
--- src/components/Main/data.jsx
+++ src/components/Main/data.jsx
@@ -3,17 +3,17 @@ import uuid from 'react-uuid';
 // color list
 export const color = ['#A7958B', '#BFAD76', '#E7D3A7', '#F0B694', '#C2C2BB']
 
-// paymentInfo(測試資料)
-export const paymentInfo = [
-    {
-        account: 
+// 測試資料
+
+// paymentData(Test) 假資料
+export const paymentDataTest = [
         {
             id: uuid,
             name: "tina",
             color: "#BFAD76",
             payment: [
                 {
-                    price: 110,
+                    price: 750,
                     item: 'MRT',
                     id: uuid
                 },
@@ -28,10 +28,7 @@ export const paymentInfo = [
                     id: uuid
                 }
             ]
-        }
-    },
-    {
-        account: 
+        },
         {
             id: uuid,
             name: "barney",
@@ -53,10 +50,7 @@ export const paymentInfo = [
                     id: uuid
                 }
             ]
-        }
-    },
-    {
-        account: 
+        },
         {
             id: uuid,
             name: "luffy",
@@ -73,9 +67,36 @@ export const paymentInfo = [
                     id: uuid
                 }
             ]
+        },
+        {
+            id: uuid,
+            name: "miumiu",
+            color: "#F0B694",
+            payment: [
+                {
+                    price: 450,
+                    item: 'candy',
+                    id: uuid
+                },
+                {
+                    price: 500,
+                    item: 'bag',
+                    id: uuid
+                }
+            ]
         }
-    }
 ]
 
-// total price(測試資料)
-export const totalPrice = paymentInfo.map((person) => person.account.payment.map((payment) => payment.price)).flat()
+// 彙整 paymentData(Test) 資料成 paymentResultData(Test)
+export const paymentResultDataTest = paymentDataTest.map((person) => {
+    const sum = person.payment.map((payment)=> payment.price).reduce((total, price) => {
+      return total + price;
+    }, 0);
+    return { name: person.name, color: person.color, payment: sum };
+  });
+
+// 從 paymentResultData(Test) 陣列中取出每個人花的錢加總 = 總花費
+export const totalPriceTest = paymentResultDataTest.map(person => person.payment).reduce((a, b)=>a+b)
+
+ // 平均一人的花費
+ export const paymentAverageTest = Math.round(totalPriceTest/paymentResultDataTest.length)

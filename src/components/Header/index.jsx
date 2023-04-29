4ec7066f8bae629be901b31af5bf75d0aa43ddc6
a03993 <a03993@yahoo.com.tw>
2023-04-29 12:16:44 +0800

    component Header update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -29,13 +29,11 @@ function Header( {currentPage, setCurrentPage} ) {
        <header>
             <Navbar className={Style.navbar} expanded={expanded} expand="lg">
                 <Container className={Style.container}>
-                    <Navbar.Brand className={Style.logo} onClick={handleHomeClick}>ShareMoney</Navbar.Brand>
+                    <Navbar.Brand className={Style.brand} onClick={handleHomeClick}>ShareMoney</Navbar.Brand>
                     <Navbar.Toggle className={Style.toggle} onClick={() => setExpanded(!expanded)} style={currentPage==='Home' ? {display: 'none'} : {display: 'block'}} aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className={Style.nav} style={currentPage==='Home' ? {display: 'none'} : {display: 'block'}}>
-                            <Nav.Link className={Style.nav__item} onClick={handleListClick}>list</Nav.Link>
-                            <Nav.Link className={Style.nav__item} onClick={handleResultClick}>result</Nav.Link>
-                        </Nav>
+                    <Navbar.Collapse className={Style.collapse}>
+                        <Nav.Link className={Style.nav__item} onClick={handleListClick} style={currentPage==='Home' ? {display: 'none'} : {display: 'block'}}>list</Nav.Link>
+                        <Nav.Link className={Style.nav__item} onClick={handleResultClick} style={currentPage==='Home' ? {display: 'none'} : {display: 'block'}}>result</Nav.Link>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
